ee957dbd08fcbe885cbfc149b60c3d012bc1512f
Eugene Ilyin <evgen.ilyin@gmail.com>
2020-06-04 15:34:05 +0300

    Initial setup.

---
+++ Surprise Me Test/AppDelegate/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Surprise Me Test
+//
+//  Created by Eugene Ilyin on 04.06.2020.
+//  Copyright © 2020 Eugene Ilyin. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    // MARK: - Properties
+    
+    var window: UIWindow?
+
+    // MARK: - UIApplicationDelegate methods
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+}
+
---
+++ Surprise Me Test/View controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Surprise Me Test
+//
+//  Created by Eugene Ilyin on 04.06.2020.
+//  Copyright © 2020 Eugene Ilyin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
